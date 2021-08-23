ce495ea08b6afcf3e5b99113cecac6e0905160b9
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-23 11:52:33 +0530

    feat:User Registration Using MsTest

---
+++ UserRegistration_MsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistration_MsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Using MsTest !");
+        }
+    }
+}
